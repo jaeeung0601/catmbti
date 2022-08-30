df3c9f3e66636f37d49b8645dfd48213931c0820
jaeeung0601 <choijaee9@gmail.com>
2022-08-30 19:49:15 +0900

    catmbti

--- src/Component/KakaoShareButton.js
+++ src/Component/KakaoShareButton.js
@@ -3,38 +3,37 @@ import Button from 'react-bootstrap/Button'
 const {Kakao} = window;
 
 const KakaoShareButton = ({data}) =>{
-    const url = "https://catmbtichoi.netlify.app /"
+    const url = "https://catmbtichoi.netlify.app/"
     const resultUrl = window.location.href;
     console.log('data',data)
-    console.log('aaa',resultUrl,url)
     React.useEffect(()=>{
         Kakao.cleanup()
-        Kakao.init("273dda4e026cb384898a183fcdd4c678")
+        Kakao.init("1b4c9d5e78a3eeb164d2cdbcf8c1909b")
         console.log(Kakao.isInitialized)
     },[])
     const shareKakao = () =>{
-      Kakao.Share.sendDefault({
-          objectType: 'feed',
-          content: {
-            title: '예비집사 판별기 결과',
-            description: `예비 집사님이 고양이를 키운다면 가장 잘 맞는 고양이는 ${data.name} 입니다.`,
-            imageUrl: url + data.image,
-            link: {
-              mobileWebUrl: resultUrl,
-              webUrl : resultUrl
-            },
+    Kakao.Share.sendDefault({
+        objectType: 'feed',
+        content: {
+          title: '예비집사 판별기 결과',
+          description: `예비 집사님이 고양이를 키운다면 가장 잘 맞는 고양이는 ${data.name}입니다.`,
+          imageUrl: url+data.image ,
+          link: {
+            mobileWebUrl: resultUrl,
+            webUrl : resultUrl
           },
-          buttons: [
-            {
-              title: '나도 테스트 하러가기',
-              link: {
-                mobileWebUrl: url,
-                webUrl: url
-              },
-            }
-          ]
+        },
+        buttons: [
+          {
+            title: '나도 테스트 하러가기',
+            link: {
+              mobileWebUrl: url,
+              webUrl : url
+            },
+          }
+        ]
       });
     }
-    return(<Button style={{marginLeft:10}} onClick={shareKakao}>카카오톡 공유하기</Button>)
+      return(<Button onClick={shareKakao} style={{marginLeft:10}}>카카오톡 공유하기</Button>)
 }
 export default KakaoShareButton
