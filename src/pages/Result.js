e1e4219f8de5ae20904ba5b045a711e99e39744d
jaeeung0601 <choijaee9@gmail.com>
2022-08-30 19:26:51 +0900

    catmbti

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -31,7 +31,7 @@ const Result = () =>{
             <Desc>예비 집사님과 찰떡궁합인 고양이는 {resultData.name}입니다.</Desc>
             <ButtonGroup>
             <Button onClick={()=>navigate('/')}>테스트 다시하기</Button>
-            <KakaoShareButton data={resultData} />
+            <KakaoShareButton data={resultData} />카카오톡 공유하기
             </ButtonGroup>
         </Wrapper>
     )
